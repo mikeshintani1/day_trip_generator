4f0f4d54e3a9ad6009395c631cb2ec57e1dfe69f
mikeshintani1 <101653682+mikeshintani1@users.noreply.github.com>
2022-03-16 07:11:38 -0700

    Update day_trip_comp.py

--- day_trip_comp.py
+++ day_trip_comp.py
@@ -30,7 +30,7 @@ def day_trip(destination, restaurant, transportation, entertainment):
     if satisfied == "y":
             print(f"You will be travelling to {destination} by {transportation}. You get to eat at {restaurant} and go {entertainment} for fun!!")
     while satisfied == "n":
-        change = input("Type 1 for change destination, 2 for restaurant, 3 for transportation or 4 for entertainment. Press 0 for no more changes: ").lower()
+        change = input("Type 1 to change destination, 2 for restaurant, 3 for transportation or 4 for entertainment. Press 0 for no more changes: ").lower()
         if change == "1":
             destination = list_loop(destination_list)
         elif change == "2":
@@ -40,8 +40,8 @@ def day_trip(destination, restaurant, transportation, entertainment):
         elif change == "4":
             entertainment = list_loop(entertainment_list)
         elif change == '0':
-            print(f"You will be travelling to {destination} by {transportation}. You get to eat at {restaurant} and go {entertainment} for fun!!")
             satisfied = input("Are you satisfied with your choices? y/n: ")
+            print(f"You will be travelling to {destination} by {transportation}. You get to eat at {restaurant} and go {entertainment} for fun!!")
             return
 
 day_trip(final_destination,final_restaurant,final_transportation,final_entertainment)
